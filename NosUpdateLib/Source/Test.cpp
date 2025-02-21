e8a7798be091a59a008cc12919dfc9743e0c1263
Adam <adam882200@gmail.com>
2025-02-21 22:24:58 +0000

    Added NosLib and boost

--- NosUpdateLib/Source/Test.cpp
+++ NosUpdateLib/Source/Test.cpp
@@ -1,7 +1,16 @@
 #include <NosUpdate/Test.hpp>
+
+#include <NosLib/HttpClient.hpp>
+
 #include <iostream>
 
 void NosUpdate::TestString()
 {
+	using NosClient = NosLib::HttpClient;
+
+	NosClient::ptr testClient = NosClient::MakeClient("https://dicksuckfor5bucks.com");
+	httplib::Result res = testClient->Get("/");
+
+	printf("dicksuckfor5bucks.com reply: %s\n", res->body.c_str());
 	printf("From Update Library\n");
 }
---
+++ NosUpdater/UpdaterMain.cpp
@@ -0,0 +1,13 @@
+#include <NosUpdate/Test.hpp>
+
+#include <iostream>
+
+int main()
+{
+	printf("From Updater\n");
+
+	NosUpdate::TestString();
+
+	printf("Press any button to continue"); getchar();
+	return 0;
+}
